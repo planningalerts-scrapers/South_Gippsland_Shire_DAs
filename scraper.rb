6ebec04a26129e897ac8721384bf3820a59062e5
Matthew Landauer <mlandauer@gmail.com>
2019-05-16 17:48:50 +1000

    Now we can completely lean on the library

--- scraper.rb
+++ scraper.rb
@@ -1,15 +1,8 @@
 require 'epathway_scraper'
 
-info_url = 'https://eservices.southgippsland.vic.gov.au/ePathway/ePathProd/Web/GeneralEnquiry/EnquiryLists.aspx?ModuleCode=LAP'
-
 scraper = EpathwayScraper::Scraper.new(
-  base_url: info_url,
+  base_url: 'https://eservices.southgippsland.vic.gov.au/ePathway/ePathProd/Web/GeneralEnquiry/EnquiryLists.aspx?ModuleCode=LAP',
   index: 0
 )
 
-page = scraper.pick_type_of_search
-
-scraper.scrape_index_page(page) do |record|
-  puts "Storing " + record['council_reference'] + " - " + record['address']
-  ScraperWiki.save_sqlite(['council_reference'], record)
-end
+scraper.scrape_and_save
