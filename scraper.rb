dc618b662485fae07b63eea8413a950763a7c65a
Matthew Landauer <mlandauer@gmail.com>
2019-05-30 10:11:19 +1000

    Use more recent version of gem

--- scraper.rb
+++ scraper.rb
@@ -1,8 +1,6 @@
 require 'epathway_scraper'
 
-scraper = EpathwayScraper::Scraper.new(
-  base_url: 'https://eservices.southgippsland.vic.gov.au/ePathway/ePathProd/Web/GeneralEnquiry/EnquiryLists.aspx?ModuleCode=LAP',
-  index: 0
+EpathwayScraper::Scraper.scrape_and_save(
+  "https://eservices.southgippsland.vic.gov.au/ePathway/ePathProd",
+  list_type: :advertising
 )
-
-scraper.scrape_and_save
