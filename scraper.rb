7e0bcf83ac49fab9b16dec729eef77a9680b0e1f
Matthew Landauer <mlandauer@gmail.com>
2019-06-05 11:54:21 +1000

    Upgrade to latest version of gem

--- scraper.rb
+++ scraper.rb
@@ -2,5 +2,5 @@ require 'epathway_scraper'
 
 EpathwayScraper.scrape_and_save(
   "https://eservices.southgippsland.vic.gov.au/ePathway/ePathProd",
-  list_type: :advertising
+  list_type: :advertising, state: "VIC"
 )
